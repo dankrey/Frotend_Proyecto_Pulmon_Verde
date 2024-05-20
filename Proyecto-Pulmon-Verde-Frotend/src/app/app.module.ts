b1958b20fe6661fdf10b972f4225ea7fae4449c9
Brian David Villamil Rodriguez <113486207+BrianBR13@users.noreply.github.com>
2024-05-20 14:41:56 -0500

    Login

--- Proyecto-Pulmon-Verde-Frotend/src/app/app.module.ts
+++ Proyecto-Pulmon-Verde-Frotend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -20,7 +20,8 @@ import { BlogComponent } from './shared/blog/blog.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
--- Proyecto-Pulmon-Verde-Frotend/src/app/shared/login/login.component.ts
+++ Proyecto-Pulmon-Verde-Frotend/src/app/shared/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  loginError: boolean = false;
 
+  constructor(private authService: AuthService) { }
+
+  onSubmit() {
+    if (this.authService.authenticate(this.username, this.password)) {
+      // Iniciar sesión exitosamente
+      this.loginError = false;
+      // Navegar a la página de bienvenida
+      // Ejemplo: this.router.navigate(['/bienvenida']);
+    } else {
+      // Mostrar error de inicio de sesión
+      this.loginError = true;
+    }
+  }
 }
