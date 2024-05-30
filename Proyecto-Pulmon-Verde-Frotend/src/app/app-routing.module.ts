3c8ca5a6dbde949f27c1f732e78b66fa9c1ecbe4
Brian David Villamil Rodriguez <113486207+BrianBR13@users.noreply.github.com>
2024-05-30 10:35:23 -0500

    registro y login ultimo

--- Proyecto-Pulmon-Verde-Frotend/src/app/app-routing.module.ts
+++ Proyecto-Pulmon-Verde-Frotend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/home/home.component';
 import { LoginComponent } from './shared/login/login.component';
 import { BlogComponent } from './shared/blog/blog.component';
+import { RegisterComponent } from './shared/register/register.component';
 
 
 const routes: Routes = [{path:'',redirectTo: 'home',  pathMatch: 'full'},
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
-  {path:'blog',component:BlogComponent}
+  {path:'blog',component:BlogComponent},
+  {path:'register',component:RegisterComponent}
 ];
 
 @NgModule({
