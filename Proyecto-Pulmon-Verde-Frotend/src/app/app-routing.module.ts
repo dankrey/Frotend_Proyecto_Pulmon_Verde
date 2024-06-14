f8a4a47d50676f129eedd6d86dfe78cfc4f321a7
daniel reyes <dereyes@uniboyaca.edu.co>
2024-06-14 12:43:20 -0500

    Revert "Merge pull request #3 from dankrey/main"
    This reverts commit 7d329ad66df1ed4983bfd355193e08c2abaef00a, reversing
    changes made to ee059da4fd13d8937020ea43512a135d6f800823.

--- Proyecto-Pulmon-Verde-Frotend/src/app/app-routing.module.ts
+++ Proyecto-Pulmon-Verde-Frotend/src/app/app-routing.module.ts
@@ -1,22 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/home/home.component';
-import { LoginComponent } from './shared/login/login.component';
-import { BlogComponent } from './shared/blog/blog.component';
-import { ContactoComponent } from './shared/contacto/contacto.component';
-import { QuienesSomosComponent } from './shared/quienes-somos/quienes-somos.component';
-import { RegisterComponent } from './shared/register/register.component';
-import { MapaComponent } from './shared/mapa/mapa.component';
-
 
 const routes: Routes = [{path:'',redirectTo: 'home',  pathMatch: 'full'},
-  {path:'home',component:HomeComponent},
-  {path:'login',component:LoginComponent},
-  {path:'blog',component:BlogComponent},
-  {path:'contacto',component:ContactoComponent},
-  {path:'QuienesSomos',component:QuienesSomosComponent},
-  {path:'Register',component:RegisterComponent},
-  {path:'Mapa',component:MapaComponent},
+  {path:'home',component:HomeComponent}
 ];
 
 @NgModule({
